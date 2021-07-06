44057f825e9616ab687f0d775804a226bdf4e996
Thomas Haferlach <t.haferlach@gmail.com>
2021-07-06 22:06:21 +0200

    Add assets to repo

--- download.sh
+++ download.sh
@@ -1,8 +1,3 @@
-wget https://i.ytimg.com/vi/nG3nyAR6bs4/hqdefault.jpg
-wget https://catfriendly.com/wp-content/uploads/2019/06/AdobeStock_53119595.jpeg
-wget https://vsitzmann.github.io/siren/img/audio/gt_bach.wav
+
 wget https://github.com/AndreyGuzhov/AudioCLIP/releases/download/v0.1/AudioCLIP-Full-Training.pt
 wget https://github.com/AndreyGuzhov/AudioCLIP/releases/download/v0.1/bpe_simple_vocab_16e6.txt.gz -O 'AudioCLIP/assets/bpe_simple_vocab_16e6.txt.gz'
-
-pip install git+https://github.com/nielsrolf/siren-pytorch 
-git clone https://github.com/nielsrolf/AudioCLIP
